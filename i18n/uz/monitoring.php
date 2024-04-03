d031cd7de7689fe18abe877e4c33e75308cfe5c9
Bahodir <41505081+st1ng2@users.noreply.github.com>
2024-04-03 21:34:38 +0500

    Added Uzbek translation

---
+++ i18n/uz/monitoring.php
@@ -0,0 +1,10 @@
+<?php return [
+    'our_servers' => 'Bizning serverlarimiz',
+    'map' => 'Xarita',
+    'server_name' => 'Server nomi',
+    'players' => 'Oʻyinchilar',
+    'connect' => 'Ulanish',
+    'widgets' => [
+        'description' => 'Koʻrsatish turi'
+    ]
+];
